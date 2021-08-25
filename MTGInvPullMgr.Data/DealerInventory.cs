db1e6d4ed28a4aa7c9afae542439cd9c579642f7
Joshua Ziegler <joshua.ziegler83@gmail.com>
2021-08-24 20:39:47 -0400

    Second Josh Update

--- MTGInvPullMgr.Data/DealerInventory.cs
+++ MTGInvPullMgr.Data/DealerInventory.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MTGInvPullMgr.Data
 {
-    class DealerInventory
+    public class DealerInventory
     {
+        [Key]
+        public int SKU { get; set; }
+        public int ApiObjectId { get; set; }
+        public int CurrentInventory { get; set; }
+        public string SetName { get; set; }
+        public string Set { get; set; }
+        public int CollectorNumber { get; set; }
+        public bool IsFoil { get; set; }
+        public bool IsVariant { get; set; }
     }
 }
