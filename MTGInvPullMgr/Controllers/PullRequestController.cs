7a60e5a803d956065f8dfa1542365ae82b101f98
John Peters <petersjohn0109@gmail.com>
2021-09-06 17:14:34 -0400

    Committing saves

---
+++ MTGInvPullMgr/Controllers/PullRequestController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Identity;
+using MTGInvPullMgr.Models;
+using MTGInvPullMgr.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MTGInvPullMgr.Controllers
+{
+    [Authorize]
+    public class PullRequestController : ApiController
+    {
+        private PullRequestServices PullRequestServices()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var pullRequestServices = new PullRequestServices(userId);
+            return pullRequestServices;
+        }
+
+        public IHttpActionResult Post(PullRequestCreate pullRequestCreate)
+        {
+
+        }
+        {
+
+        }
+    }
+}
